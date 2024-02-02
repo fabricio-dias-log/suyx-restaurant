ed04e0a5d9f277109bc284ae54b8403a03fa5324
Fabricio Dias <fabricio270909hotmail.com>
2024-02-02 10:56:09 -0300

    Lists of cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import hashtauranteImg from './assets/hashtaurante.webp';
 import './App.css';
 import  Navegacao from './Navegacao';
 import ItemCardapio from './ItemCardapio';
+import { pratosPrincipais, sobremesas, bebidas } from './cardapio';
 
 export default function App() {
-  
-
   return (
     <div>
       <img src={hashtauranteImg} className="capa"></img>
       <Navegacao />
       <div className='menu'>
-        <ItemCardapio />
-        <ItemCardapio />
-        <ItemCardapio />
-        <ItemCardapio />
+        {pratosPrincipais.map(item => 
+          <ItemCardapio 
+          nome={item.nome} 
+          descricao={item.descricao} 
+          preco={item.preco} 
+          imagem={item.imagem}
+          />
+        )}
       </div>
     </div>
   )
